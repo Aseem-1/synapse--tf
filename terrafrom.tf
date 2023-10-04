8dcc1973c74d1279571330f3efa3d73595f98f8f
A533M <aseemsuleman74@gmail.com>
2023-10-04 14:00:52 +0100

    update

--- terrafrom.tf
+++ terrafrom.tf
@@ -9,11 +9,11 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
-    ARM_CLIENT_ID: ${{ secrets.AZURE_AD_CLIENT_ID }}
-    ARM_CLIENT_SECRET: ${{ secrets.CLIENT_SECRET }}
-    ARM_SUBSCRIPTION_ID: ${{ secrets.AZURE_SUBSCRIPTION_ID }}
-    ARM_TENANT_ID: ${{ secrets.AZURE_AD_TENANT_ID }}
+
+    ARM_CLIENT_ID = secrets.AZURE_AD_CLIENT_ID 
+    ARM_CLIENT_SECRET = secrets.CLIENT_SECRET 
+    ARM_SUBSCRIPTION_ID = secrets.AZURE_SUBSCRIPTION_ID
+    ARM_TENANT_ID = secrets.AZURE_AD_TENANT_ID
 }
 
 resource "azurerm_resource_group" "example" {
