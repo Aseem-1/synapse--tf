87f5b697796ec251dd17bd6ef6aa5fa14c8acefa
A533M <aseemsuleman74@gmail.com>
2023-10-04 13:15:51 +0100

    update

--- terrafrom.tf
+++ terrafrom.tf
@@ -8,17 +8,13 @@ terraform {
 }
 
 provider "azurerm" {
-<<<<<<< HEAD
   features {}
 
   subscription_id   = "2bd92727-1a9b-4d70-b27d-e3de734c45a8"
   tenant_id         = "6f1a44dc-99c5-462d-b27d-4a6926a6b3e0"
   client_id         = "acf5f505-5481-4682-9ecb-a0d48eb01984"
   client_secret     = "TPL8Q~SszJUJNq8D1WF2Im9Y7roUXPNGnmPGmcoq"
-=======
-  # Configuration options
-    creds: ${{ secrets.AZURE_CREDENTIALS }}
->>>>>>> 3d9bc97e303cf410dad4add71398b26aaf668bdb
+
 }
 
 resource "azurerm_resource_group" "example" {
